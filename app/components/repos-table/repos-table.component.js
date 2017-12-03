7c55209f0374e85727e0fa8f846b66bb897060c7
Myroslava Haidash <myroslava.haidash@gmail.com>
2017-12-04 01:42:46 +0200

    Improve UI

--- app/components/repos-table/repos-table.component.js
+++ app/components/repos-table/repos-table.component.js
@@ -8,15 +8,23 @@ angular.module('githubSearchApp').component('reposTable', {
         this.currentPage = 1;
         this.itemsPerPage = 10;
         this.searchQuery = '';
+        this.isLoading = false;
+        this.error = false;
 
         this._loadData = function() {
             var maxAllowedGithubResults = 1000;
 
             if(this.searchQuery){
+                this.isLoading = true;
+                this.error = false;
                 githubClient.getRepos(this.searchQuery, this.currentPage, this.itemsPerPage).then(function(response) {
+                    self.isLoading = false;
                     self.results = response.data.items;
                     self.totalItems =
                         response.data.total_count <= maxAllowedGithubResults ? response.data.total_count : maxAllowedGithubResults;
+                }).catch(function(){
+                    self.error = true;
+                    self.isLoading = false;
                 });
             } else {
                 this.results = [];
